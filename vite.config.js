a564d2dfdf7e7f36470fa24b1fd4006fb401ded3
JeongSho <jc366@naver.com>
2024-08-02 07:31:35 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/your-repo-name/', // 리포지토리 이름으로 변경하세요
+  base: '/dr_menu/', // 리포지토리 이름으로 변경하세요
   build: {
     outDir: 'dist',
   },
